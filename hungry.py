bc6a62446f6b35943dd0e6975e0d51b1c78051c9
rshubh <43298840+rshubh@users.noreply.github.com>
2018-10-26 23:51:15 +0530

    ADD by me
    If it is start capital letter then

--- hungry.py
+++ hungry.py
@@ -2,6 +2,12 @@ hungry=input("are you hungry?")
 if hungry=="yes" and hungry=="Y":
     print("eat samosa")
     print("eat pizza")
-    print("eat burger")
+    print("eat chowmein")
+    print("eat burger") 
+elif hungry.upper()=="yes" and hungry.upper()=="Y":
+    print("eat samosa")
+    print("eat pizza")
+    print("eat chowmein")
+    print("eat burger")    
 else:
     print("do homework")    
